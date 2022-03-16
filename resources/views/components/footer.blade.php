3204192d6c00751c034390caa0ffa60bcb4f93af
Fuat Akbar <fuatakbars@gmail.com>
2022-03-16 10:52:00 +0700

    Feat: homepage product display

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,10 @@
 <footer class="container">
     <div class="row justify-content-center">
-        <div class="col-8 copyright">
-            <p class="text-center">Simple Shop &copy; 2022. By <a href="https://www.linkedin.com/in/fuatakbar/">Fuat Akbar</a></p>
+        <div class="col-10 copyright">
+            <p class="text-center">Simple Shop &copy; 2022. By <a href="https://www.linkedin.com/in/fuatakbar/" target="_blank">
+                    Fuat Akbar
+                </a>
+            </p>
         </div>
     </div>
 </footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,103 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        <div class="col-md-10">
+            <div class="row justify-content-start products">
+                <div class="col-12 col-md-4 items">
+                    <a href="#">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="card-header">
+                                    <img src="https://th.bing.com/th/id/OIP.41WKPihA_v0vW3WFwhXg9wHaJT?pid=ImgDet&rs=1" class="card-img-top" alt="...">
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <p class="name">LARAVEL "HAMBINO" BASEBALL CAP</p>
+                                <hr>
+                                <p class="price">IDR 50.000</p>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </a>
+                </div>
+                <div class="col-12 col-md-4 items">
+                    <a href="#">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="card-header">
+                                    <img src="https://th.bing.com/th/id/OIP.41WKPihA_v0vW3WFwhXg9wHaJT?pid=ImgDet&rs=1" class="card-img-top" alt="...">
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <p class="name">LARAVEL "HAMBINO" BASEBALL CAP</p>
+                                <hr>
+                                <p class="price">IDR 50.000</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-12 col-md-4 items">
+                    <a href="#">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="card-header">
+                                    <img src="https://th.bing.com/th/id/OIP.41WKPihA_v0vW3WFwhXg9wHaJT?pid=ImgDet&rs=1" class="card-img-top" alt="...">
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <p class="name">LARAVEL "HAMBINO" BASEBALL CAP</p>
+                                <hr>
+                                <p class="price">IDR 50.000</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-12 col-md-4 items">
+                    <a href="#">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="card-header">
+                                    <img src="https://th.bing.com/th/id/OIP.41WKPihA_v0vW3WFwhXg9wHaJT?pid=ImgDet&rs=1" class="card-img-top" alt="...">
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <p class="name">LARAVEL "HAMBINO" BASEBALL CAP</p>
+                                <hr>
+                                <p class="price">IDR 50.000</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-12 col-md-4 items">
+                    <a href="#">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="card-header">
+                                    <img src="https://th.bing.com/th/id/OIP.41WKPihA_v0vW3WFwhXg9wHaJT?pid=ImgDet&rs=1" class="card-img-top" alt="...">
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <p class="name">LARAVEL "HAMBINO" BASEBALL CAP</p>
+                                <hr>
+                                <p class="price">IDR 50.000</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-12 col-md-4 items">
+                    <a href="#">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="card-header">
+                                    <img src="https://th.bing.com/th/id/OIP.41WKPihA_v0vW3WFwhXg9wHaJT?pid=ImgDet&rs=1" class="card-img-top" alt="...">
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <p class="name">LARAVEL "HAMBINO" BASEBALL CAP</p>
+                                <hr>
+                                <p class="price">IDR 50.000</p>
+                            </div>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
