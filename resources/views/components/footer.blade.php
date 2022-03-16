43dd704c55c1688734ec2fff4c23082582153125
Fuat Akbar <fuatakbars@gmail.com>
2022-03-16 10:06:51 +0700

    Fix: footer width on mobile

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8 copyright">
+        <div class="col-8 copyright">
             <p class="text-center">Simple Shop &copy; 2022. By <a href="https://www.linkedin.com/in/fuatakbar/">Fuat Akbar</a></p>
         </div>
     </div>
